00aedcb2aa204628ff1b5925e126a80062ed098a
Yihe Dong <yihed@wolfram.com>
2016-04-25 18:28:26 -0500

    tiny update to Struct classes

---
+++ thmp/ParseToWL.java
@@ -0,0 +1,22 @@
+package thmp;
+
+
+/**
+ * Transforms parse tree to WL
+ * @author yihed
+ *
+ */
+
+public class ParseToWL {
+	
+	/**
+	 * 
+	 * @param headStruct is head of a parse
+	 */
+	public static void parseToWl(Struct headStruct){
+		
+		
+	}
+	
+	
+}
