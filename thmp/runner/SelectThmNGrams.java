f8f8a0ebb64bc246fd8844a25f9e3e8b170d2b23
Yihe Dong <yihed@wolfram.com>
2017-12-11 14:18:20 -0600

    improved 4 gram collection

--- thmp/runner/SelectThmNGrams.java
+++ thmp/runner/SelectThmNGrams.java
@@ -3,6 +3,7 @@ package thmp.runner;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -13,6 +14,7 @@ import java.util.regex.Pattern;
 
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
+import com.google.common.collect.TreeMultiset;
 import com.wolfram.puremath.dbapp.DBUtils;
 import com.wolfram.puremath.dbapp.SimilarThmUtils;
 
@@ -29,9 +31,10 @@ import thmp.utils.WordForms;
 public class SelectThmNGrams {
 
 	//number of thms to serialize at a time
-	private static final int numThmPerBundle = 5000;
+	private static final int numThmPerBundle = 50000;
 		
-	private static final Pattern THM_SCRAPE_PUNCTUATION_PATTERN = Pattern.compile("(?:(?=[,!:;.\\s])|(?<=[,!:;.\\s]))");
+	private static final Pattern WHITE_SPACE_PATT = WordForms.getWhiteEmptySpacePattern();
+	private static final Pattern THM_SCRAPE_PUNCTUATION_PATTERN = Pattern.compile("(?:(?=[,!:;.])|(?<=[,!:;.])|\\s+)");
 	private static final Pattern THM_SCRAPE_ELIM_PUNCTUATION_PATTERN = Pattern
 			.compile("[\\{\\[\\)\\(\\}\\]$\\%/|#@.;,:_~!&\"`+<>=#]");
 	private static final Set<String> SCRAPE_STOP_WORDS_BEFORE_SET = new HashSet<String>();
@@ -44,6 +47,28 @@ public class SelectThmNGrams {
 		}	
 	}
 	
+	private static class Comp implements Comparator<String>{
+		
+		Multiset<String> mset;
+		public Comp(Multiset<String > s) {
+			this.mset=s;
+		}
+		
+		@Override
+		public int compare(String a, String b) {
+			
+			if(!mset.contains(a)) {
+				return 1;
+			}
+			if(!mset.contains(b)) {
+				return -1;
+			}
+			int count1 = mset.count(a);
+			int count2 = mset.count(b);
+			return count1 < count2 ? 1 : count2 < count1 ? -1 : 0;
+		}
+	}
+	
 	public static void main(String[] args) {		
 		selectNGrams();
 	}
@@ -53,40 +78,55 @@ public class SelectThmNGrams {
 	 */
 	private static void selectNGrams() {
 		
-		
 		int totalThmCount = ThmHypPairGet.totalThmsCount();
 		int totalBundles = (int)Math.ceil(((double)totalThmCount) / numThmPerBundle);
+		//int totalThmCount = 1;
+		//int totalBundles = 1;
 		
-		Multiset<String> nGramMSet = HashMultiset.create();
-		
+		String rootPath = "src/thmp/data/nGramScrape";
 		//1/3 way through, stop adding new ones, for memory purposes, and only add count if already contained
+		int collectCap = totalBundles/3;
+		collectCap = collectCap == 0 ? 1 : collectCap;
 		
 		for(int j = 0; j < totalBundles; j++) {
 			
-			boolean addNewTerms = j < totalBundles/3;
+			boolean addNewTerms = j < collectCap;
+			addNewTerms = true;
+			Multiset<String> nGramMSet = HashMultiset.create();
 			
-			Map<Integer, byte[]> similarThmsMap = new HashMap<Integer, byte[]>();
 			int endingIndex = Math.min(totalThmCount, (j+1)*numThmPerBundle);
 			int startingIndex = j * numThmPerBundle;
-			for(int i = startingIndex; i < endingIndex; i++) {	
+			
+			for(int i = startingIndex; i < endingIndex
+					; i++) {
 				
 				ThmHypPair thmHypPair = ThmHypPairGet.retrieveThmHypPairWithThm(i);
 				String thmStr = thmHypPair.getEntireThmStr();
+				//String thmStr = "hi there hdsh wt bunny sf gsg sgrgd dhg sf gsg sgrgd dhg";
+				//thmStr = "hi there hds khl, jkk$ agsg hi there hds khl. hjkh kh hk gh";
 				select4Grams(thmStr, nGramMSet, addNewTerms);
 				
-				List<Integer> similarThmList = thmp.search.SimilarThmSearch.preComputeSimilarThm( i );
-				similarThmsMap.put(i, SimilarThmUtils.indexListToByteArray(similarThmList));
-				
-				
 			}
+			//System.out.println("nGramMSet "+nGramMSet);
 			
+			TreeMultiset<String> sortedMSet = TreeMultiset.create(new Comp(nGramMSet));
+			sortedMSet.addAll(nGramMSet);
 			
-			String path = DBUtils.SimilarThmsTb.similarThmIndexByteArrayPathNoDat + j + ".dat" ;
+			List<String> list = new ArrayList<String>(sortedMSet.elementSet());
+			
+			//list.sort(new Comp(nGramMSet));
+			
+			//System.out.println("list: " + list);
+			
 			//make map instead of write to file.
-			List<Map<Integer, byte[]>> similarThmsMapList = new ArrayList<Map<Integer, byte[]>>();
-			similarThmsMapList.add(similarThmsMap);
+			List<List<String>> similarThmsMapList = new ArrayList<List<String>>();
+			similarThmsMapList.add(list);
+			String path = rootPath + j + ".dat";
 			thmp.utils.FileUtils.serializeObjToFile(similarThmsMapList, path);
-			System.out.println("Done serializing for bundle " + j);
+			String txtPath = rootPath + j + ".txt";
+			
+			thmp.utils.FileUtils.writeToFile(sortedMSet, txtPath);
+			
 		}
 		
 	}
@@ -100,26 +140,35 @@ public class SelectThmNGrams {
 			boolean allowNewWords) {
 		
 		String thmLower = thm.toLowerCase();
-		List<String> lineList = Arrays.asList(THM_SCRAPE_PUNCTUATION_PATTERN.split(thmLower));
-		
+		List<String> wordsList = Arrays.asList(THM_SCRAPE_PUNCTUATION_PATTERN.split(thmLower));
+		//System.out.println("lineList "+lineList);
 		//gather words, stop at punctuations, stop words, and latex.
-		Iterator<String> iter = lineList.iterator();
+		//Iterator<String> iter = lineList.iterator();
+		int wordsListSz = wordsList.size();
+		//int startingIndex = 0;
+		int index = 0;
 		
-		outerWhile: while(iter.hasNext()) {
+		outerWhile: for(int startingIndex = 0; startingIndex < wordsListSz; startingIndex++) {
 			
 			StringBuilder sb = new StringBuilder(30);
-			for(int j = 0; j < 4; j++) {
-				if(!iter.hasNext()) {
+			int wordCount = 0;
+			
+			index = startingIndex;
+			while(wordCount < 4 ) {
+				if(index >= wordsListSz) {
 					break outerWhile;
 				}
-				String word = iter.next();
-				//note could have tex!
+				
+				String word = wordsList.get(index);
+				if(WHITE_SPACE_PATT.matcher(word).matches()) {	
+					index++;
+					continue;
+				}
+				//one way to skip tex
 				if(word.length() < 2) {
 					continue outerWhile;
 				}
-				if(WordForms.getWhiteEmptySpacePattern().matcher(word).matches()) {				
-					continue outerWhile;
-				}
+				
 				if(THM_SCRAPE_ELIM_PUNCTUATION_PATTERN.matcher(word).find()) {
 					continue outerWhile;
 				}				
@@ -127,6 +176,8 @@ public class SelectThmNGrams {
 					continue outerWhile;
 				}
 				sb.append(word).append(" ");
+				wordCount++;
+				index++;
 			}
 			String word = sb.toString();
 			if(!allowNewWords) {
@@ -137,87 +188,6 @@ public class SelectThmNGrams {
 				nGramMSet.add(word);
 			}
 		}
-		
 	}
-	/*
-	private static void scrapeThmNames(String line, List<String> thmNameList) {
-			
-			List<String> lineList = Arrays.asList(THM_SCRAPE_PUNCTUATION_PATTERN.split(line));
-			int lineListSz = lineList.size();
-			for(int i = 0; i < lineListSz; i++) {
-				String word = lineList.get(i);
-				
-				
-				if(THM_SCRAPE_PATTERN.matcher(word).matches()) {
-					String thmWords = collectThmWordsBeforeAfter(lineList, i);
-					if(!WHITE_SPACE_PATTERN.matcher(thmWords).matches()) {
-						thmNameList.add(thmWords);							
-					}
-				}
-			}			
-		}
-	
-	
-	private static String collectThmWordsBeforeAfter(List<String> thmList, int index) {
-		int indexBoundToCollect = 6;
-		StringBuilder sb = new StringBuilder(40);
-		int thmListSz = thmList.size();
-		int i = 1;
-		int count = 0;
-		boolean gathered = false;
-		while(count < indexBoundToCollect && index - i > -1) {
-			String curWord = thmList.get(index-i);
-			if(WordForms.getWhiteEmptySpacePattern().matcher(curWord).matches()) {
-				i++;
-				continue;
-			}
-			if(THM_SCRAPE_ELIM_PUNCTUATION_PATTERN.matcher(curWord).find()) {
-				break;
-			}
-			if(THM_SCRAPE_ELIM_PATTERN.matcher(curWord).matches()) {
-				return "";
-			}
-			if(count==0 && SCRAPE_STOP_WORDS_BEFORE_SET.contains(curWord)) {
-				break;
-			}
-			sb.insert(0, curWord + " ");
-			i++;
-			count++;
-			gathered = true;
-		}
-		sb.append(thmList.get(index)).append(" ");
-		indexBoundToCollect = 7;
-		i = 1;
-		count = 0;
-		while(count < indexBoundToCollect && index + i < thmListSz) {
-			String curWord = thmList.get(index+i);
-			if(WordForms.getWhiteEmptySpacePattern().matcher(curWord).matches()) {
-				i++;
-				continue;
-			}	
-			if(THM_SCRAPE_ELIM_PUNCTUATION_PATTERN.matcher(curWord).find()) {
-				break;
-			}
-			if(count==0 && WordForms.DIGIT_PATTERN.matcher(curWord).find()) {
-				break;
-			}
-			if(THM_SCRAPE_ELIM_PATTERN.matcher(curWord).matches()) {
-				return "";
-			}
-			sb.append(curWord).append(" ");
-			i++;
-			count++;
-			gathered = true;
-		}
-		if(sb.length() > 1) {
-			sb.deleteCharAt(sb.length()-1);
-		}
-		if(gathered) {
-			return sb.toString();
-		}else {
-			return "";
-		}		
-	}
-	*/
 	
 }
